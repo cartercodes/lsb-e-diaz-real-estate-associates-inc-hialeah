cf06ddc698554d043f38a758369ebf5db20e4010
Dispatch <dispatch@cartercodes.dev>
2026-04-08 21:12:56 -0400

    Site deployment

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/lsb-e-diaz-real-estate-associates-inc-hialeah',
+  images: { unoptimized: true },
+};
+module.exports = nextConfig;
